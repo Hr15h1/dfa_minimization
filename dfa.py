fd839b91691da861003c70ffc7cdf6b1d1718a52
Hr15h1 <jyothkrishnan73@gmail.com>
2025-02-03 06:37:10 +0530

    Create function
    Created a function that will visualize an input DFA.

--- dfa.py
+++ dfa.py
@@ -9,28 +9,35 @@
 #     print(transitions)
     
 from graphviz import Digraph
-import time
+from datetime import datetime
 t_table = [[0,1,2], [1,0,2], [1,1,4], [2,1,4], [2,0,1], [3,0,2], [3,1,4], [4,0,4], [4,1,4]]
 symbols = ['0', '1']
-states = [0,1,2,3,4]
-initial_state = 0
-final_state = 4
-g = Digraph()
-for state in states:
-    g.node(
-        chr(ord('A') + state), 
-        label = chr(ord('A') + state),
-        shape = "doublecircle" if state == final_state else "circle"
-    )
+states = ['q0','q1','q2','q3','q4']
+initial_state = 'q0'
+final_state = 'q4'
 
-for t in t_table:
-    g.edge(
-        chr(ord('A') + t[0]),
-        chr(ord('A') + t[2]),
-        label = f"{t[1]}"
-    )
+def dfa_visualizer(t_table, states, initial_state, final_state):
+    g = Digraph()
+    g.attr(rankdir = 'LR')
+    g.node('start', shape = 'point', style = 'invis')
+    for state in states:
+        g.node(
+            state, 
+            label = state,
+            shape = "doublecircle" if state == final_state else "circle"
+        )
 
-g.render('dfa', 'images/', format = 'png', view = True)
+    g.edge('start', initial_state)
+    for t in t_table:
+        g.edge(
+            states[t[0]],
+            states[t[2]],
+            label = f"{t[1]}"
+        )
+
+    g.render(f'dfa_{datetime.now().strftime("%M%S")}', 'images/', format = 'png')
+
+dfa_visualizer(t_table, states, initial_state, final_state)
 
 
 
